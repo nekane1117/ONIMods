1e5da76b18431fdb667fef468f2f86e021010d62
nekane1117 <november_melody_bell@yahoo.co.jp>
2022-12-28 16:04:24 +0900

    initial commit

---
+++ ONIMods/AlwaysRequestSuitLocker/Main.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+
+namespace HelloWorld
+{
+    [HarmonyPatch(typeof(GeneratedBuildings), "LoadGeneratedBuildings")]
+    internal class HelloWorld_GeneratedBuildings_LoadGeneratedBuildings
+    {
+        private static void Postfix()
+        {
+            Debug.Log("Hello World");
+        }
+    }
+}
