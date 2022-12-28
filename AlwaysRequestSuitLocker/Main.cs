be39caeaba5a4bf92e79ba6ea170bb6dd982fb3c
nekane1117 <november_melody_bell@yahoo.co.jp>
2022-12-28 18:23:43 +0900

    work

---
+++ AlwaysRequestSuitLocker/Main.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using System;
+
+namespace AlwaysRequestSuitLocker
+{
+    [HarmonyPatch(typeof(SuitLocker), nameof(SuitLocker.DropSuit), new Type[] { })]
+    internal class DropSuitPatch
+    {
+        private static void Postfix(SuitLocker __instance)
+        {
+            AlwaysRequestSuitLockerUtil.requestSuit(__instance);
+        }
+    }
+
+    [HarmonyPatch(typeof(SuitLocker), nameof(SuitLocker.EquipTo), new Type[] { typeof(Equipment) })]
+    internal class EqiopToPatch
+    {
+        private static void Postfix(Equipment equipment, SuitLocker __instance)
+        {
+            AlwaysRequestSuitLockerUtil.requestSuit(__instance);
+        }
+    }
+}
+
+class AlwaysRequestSuitLockerUtil
+{
+    public static SuitLocker requestSuit(SuitLocker sl)
+    {
+        sl.smi.sm.isWaitingForSuit.Set(true, sl.smi);
+        return sl;
+    }
+
+}
