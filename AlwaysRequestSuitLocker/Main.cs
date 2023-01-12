e9a6022cb81035a47b244ba90a7888c24ca2cf4d
nekane1117 <november_melody_bell@yahoo.co.jp>
2023-01-12 23:08:13 +0900

    add option dialog

--- AlwaysRequestSuitLocker/Main.cs
+++ AlwaysRequestSuitLocker/Main.cs
@@ -1,6 +1,15 @@
 ﻿using HarmonyLib;
+using KMod;
+using PeterHan.PLib.Actions;
+using PeterHan.PLib.AVC;
+using PeterHan.PLib.Core;
+using PeterHan.PLib.Database;
+using PeterHan.PLib.Options;
+using PeterHan.PLib.PatchManager;
+using PeterHan.PLib.UI;
 using System;
 using System.Linq;
+using System.Reflection;
 
 namespace AlwaysRequestSuitLocker
 {
@@ -19,6 +28,27 @@ namespace AlwaysRequestSuitLocker
         private static void Postfix(Equipment equipment, StateMachineComponent __instance)
         {
             AlwaysRequestSuitLockerUtil.requestSuit(__instance);
+        }
+    }
+
+
+    public sealed class AlwaysRequestSuitLockerOptions : UserMod2
+    {
+        public override void OnLoad(Harmony harmony)
+        {
+            base.OnLoad(harmony);
+            PUtil.InitLibrary();
+            new POptions().RegisterOptions((UserMod2)this, typeof(Options));
+            new PPatchManager(harmony).RegisterPatchClass(typeof(AlwaysRequestSuitLockerOptions));
+        }
+
+        [HarmonyPatch(typeof(PlayerController), "OnPrefabInit")]
+        public static class PlayerControllerOnPrefabInitPatch
+        {
+            public static void Postfix(PlayerController __instance)
+            {
+                Globals.opt = POptions.ReadSettings<Options>() ?? new Options();
+            }
         }
     }
 }
---
+++ AlwaysRequestSuitLocker/Options.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using PeterHan.PLib.Options;
+
+namespace AlwaysRequestSuitLocker
+{
+    [JsonObject]
+    [ModInfo("https://github.com/nekane1117/ONIMods/tree/master/AlwaysRequestSuitLocker")]
+    public class Options
+    {
+        [Option("Auto request OxygenMask")]
+        public bool OxygenMask { get; set; } = true;
+
+        [Option("Auto request AtmoSuit")]
+        public bool AtmoSuit { get; set; } = true;
+
+        [Option("Auto request JetSuit")]
+        public bool JetSuit { get; set; } = false;
+    }
+}
